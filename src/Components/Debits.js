4233fb642d457480fa9c8ce4551024cdb23511a1
azheng4119 <azheng4119@bths.edu>
2019-07-29 16:23:29 -0400

    added cards

--- src/Components/Debits.js
+++ src/Components/Debits.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import DebitInput from './DebitInput';
-
+import Card from "./Card";
+import "./Debits.css"
 class Debits extends Component{
     render(){
         let debits = [];
         for (let i of this.props.debits){
-            debits.push(<div>{i.description} : {i.amount}</div>);
+            debits.push(
+            <Card name = {i.description} amount = {i.amount} date = {i.date}></Card>
+            );
         }
         return <div>
                 <Link to="">Home</Link><br></br>
-                <span><strong>Total Debits : {this.props.debitsTotal}</strong></span>
-                <span>{debits}</span>
                 <DebitInput addDebits = {this.props.addDebits}></DebitInput>
+                <span><strong>Total Debits : {this.props.debitsTotal}</strong></span>
+                <div id = "cards">{debits}</div>
         </div>
     }
 }
