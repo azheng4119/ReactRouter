443f7ac2924b5d61017b2e925b43184440492999
TJ Westfall <thomas.westfall64@myhunter.cuny.edu>
2019-07-29 21:09:13 -0400

    Updates

---
+++ precache-manifest.d7a3f5ed046f0a22742268f5eeb612d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d7c19526e8ff78b46a9a581a7a154b",
+    "url": "/reactrouter/index.html"
+  },
+  {
+    "revision": "6beafdf492b755c94644",
+    "url": "/reactrouter/static/css/main.37e40633.chunk.css"
+  },
+  {
+    "revision": "4a41fc11d92c45b433c5",
+    "url": "/reactrouter/static/js/2.6212c7b0.chunk.js"
+  },
+  {
+    "revision": "6beafdf492b755c94644",
+    "url": "/reactrouter/static/js/main.5578d442.chunk.js"
+  },
+  {
+    "revision": "412098946a40adbb48ec",
+    "url": "/reactrouter/static/js/runtime~main.0fa15701.js"
+  }
+]);
