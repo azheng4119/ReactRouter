5eefc2b876307675bfffe310c1c525a6dbfa6051
azheng4119 <azheng4119@bths.edu>
2019-07-30 14:01:21 -0400

    Updates

--- precache-manifest.a19e93347649d5c1b7be59d5e8db3b3c.js
+++ precache-manifest.a19e93347649d5c1b7be59d5e8db3b3c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f0df3dfd3cad4f119ac99805b52dda24",
+    "revision": "07196a216ed7cf6bfc00bb39fb831d15",
     "url": "/ReactRouter/index.html"
   },
   {
-    "revision": "5baa77431e992158705f",
+    "revision": "51b5121a341be7e33c2a",
     "url": "/ReactRouter/static/css/main.d9081180.chunk.css"
   },
   {
-    "revision": "ec6d3b00674aee7bf4c1",
-    "url": "/ReactRouter/static/js/2.326be1d1.chunk.js"
+    "revision": "066851669d7e272bc456",
+    "url": "/ReactRouter/static/js/2.599117b0.chunk.js"
   },
   {
-    "revision": "5baa77431e992158705f",
-    "url": "/ReactRouter/static/js/main.f6cee232.chunk.js"
+    "revision": "51b5121a341be7e33c2a",
+    "url": "/ReactRouter/static/js/main.e18207e6.chunk.js"
   },
   {
     "revision": "d22ca8337b0e2cf47f30",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactRouter/precache-manifest.1ffc2a6988ef301edc14d3bc200f2df7.js"
+  "/ReactRouter/precache-manifest.a19e93347649d5c1b7be59d5e8db3b3c.js"
 );
 
 self.addEventListener('message', (event) => {
